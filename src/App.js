258e261719a5861b0c5676b7e2acc7a1c11faa22
mfaridzia <farid19barcelona@gmail.com>
2020-03-10 22:48:14 +0700

    🚀 init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const ENDPOINT = 'https://api.rss2json.com/v1/api.json?rss_url=';
+
+const App = () => {
+  const [url, setURL] = React.useState("");
+  const [state, setState] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(false);
+
+  const handleRSSFeed = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    setError(false);
+    try {
+      const response = await fetch(`${ENDPOINT}${url}`);
+      const { items } = await response.json();
+      console.log(items)
+      setTimeout(() => {
+        setState(items);
+        setLoading(false);
+      }, 2000);
+    } catch (error) {
+      setError(true);
+      setLoading(false);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> RSS Feed </h2>
+      <input 
+        type="text" 
+        value={url}
+        onChange={e => setURL(e.target.value)}
+      />
+      <button type="submit" onClick={handleRSSFeed}> Submit </button>
+
+      <section className="lists-rss-feed">
+        { loading ? "Loading..." : 
+          <ul>
+            { state.length === 0 ? "No data to display!" :
+              state.map((item, i) => {
+                return <li key={i}> { item.title } </li>
+              })
+            }
+          </ul>
+        }
+        { error && "Error Parsing Feed, Please try again." }
+      </section>
     </div>
   );
 }
